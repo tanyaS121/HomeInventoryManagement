c68427b32e8aee4105c2943a8d87e0990fe5fa11
Tanya Sai <68801236+tanyaS121@users.noreply.github.com>
2021-01-17 21:13:27 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,13 @@
+package homeinventory;
+
+public class InventoryItem {
+    public String description;
+    public String location;
+    public String serialNumber;
+    public String purchasePrice;
+    public String purchaseDate;
+    public String purchaseLocation;
+    public String note;
+    public String photoFile;
+    public boolean marked;
+}
